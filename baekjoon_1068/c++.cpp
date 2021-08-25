6756fd769a98094705ce057656e1e07bd4d88458
jjo <k01066624566@gmail.com>
2021-08-26 07:43:34 +0900

    solve 1068 with c++

--- baekjoon_1068/c++.cpp
+++ baekjoon_1068/c++.cpp
@@ -1,55 +1,34 @@
 #include<iostream>
 #include<vector>
-using namespace  std;
-int cnt=0;
-typedef struct node * treepointer;
-typedef struct node{
-  int data;
-  treepointer left,right,parent;
-}node;//트리의 데이터,왼쪽 노드,오른쪽 노드,부모 노드가 들어있는 노드 구조체
-
-void preorder(treepointer ptr){
-  if(ptr){
-    if(ptr->left==NULL&&ptr->right==NULL){
+using namespace std;
+int erase,cnt=0;
+void dfs(vector<vector<int>> graph,int root){
+  if(root==erase){return;}
+  if(graph[root].size()==0){cnt++;return;}
+  else if(graph[root].size()==1){
+    if(graph[root][0]==erase){
       cnt++;
+      return;
     }
-    preorder(ptr->left);
-    preorder(ptr->right);
   }
-}//순회 하면서 왼쪽과 오른쪽이 NULL이면 cnt++
-
+  for(int i=0;i<graph[root].size();i++){
+    dfs(graph,graph[root][i]);
+  }
+}
+vector<vector<int>> graph;
 int main(){
-  int num;
-  scanf("%d",&num);
-  node nodes[num+1];
-  for(int i=0;i<num;i++){
-    nodes[i].data=0;
-    nodes[i].left=NULL;
-    nodes[i].right=NULL;
-  }
-
+  int num,root;
+  cin>>num;
+  graph.resize(num);
   for(int i=0;i<num;i++){
     int parent;
-    scanf("%d",&parent);
-    if(nodes[parent].left==NULL){
-      nodes[i].data=i;
-      nodes[parent].left=&nodes[i];
-      nodes[i].parent=&nodes[parent];
-    }
-    else{
-      nodes[parent].right=&nodes[i];
-      nodes[i].parent=&nodes[parent];
-    }
+    cin>>parent;
+    if(parent!=-1)
+      graph[parent].push_back(i);
+    else 
+      root=i;
   }
-
-  int erase;
-  scanf("%d",&erase);
-  if(erase==0){
-    printf("%d",cnt);
-    return 0;
-  }
-  nodes[erase].parent->left->data==nodes[erase].data?nodes[erase].parent->left=NULL:nodes[erase].parent->right=NULL;//부모 노드와의 연결을 끊어서 삭제
-
-  preorder(&nodes[0]);
-  printf("%d",cnt);
+  cin>>erase;
+  dfs(graph,root);
+  cout<<cnt<<endl;
 }
