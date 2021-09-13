11ad4946de2dc6333412f51e64fbad1135c4e8bb
jjo <k01066624566@gmail.com>
2021-09-13 15:49:29 +0900

    solve 1927 with c++

---
+++ baekjoon_1927/c++.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+#include<queue>
+#include<vector>
+using namespace std;
+int main(){
+  priority_queue<int,vector<int>,greater<int>> q;
+  int n;
+  scanf("%d",&n);
+  for(int i=0;i<n;i++){
+    int a;
+    scanf("%d",&a);
+    if(a!=0){
+      q.push(a);
+    }
+    else{
+      if(!q.empty()){
+        printf("%d\n",q.top());
+        q.pop();
+      }
+      else
+        printf("0\n");
+    }
+  }
+}
